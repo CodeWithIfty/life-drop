e37776bc5ec444ec21e1b830f464f215f1461db4
CodeWithIftekhar <codewithiftekhar@gmail.com>
2023-12-05 21:07:33 +0600

    added profile avatar card and installed react-icons

--- src/Pages/Profile/Profile.jsx
+++ src/Pages/Profile/Profile.jsx
@@ -1,7 +1,19 @@
+import ProfileAvatarCard from "./ProfileComponents/ProfileAvatarCard";
+
 const Profile = () => {
   return (
-    <div className="container mx-auto h-screen bg-red-700 p-5">
-      This is the profile Page
+    <div className="container mx-auto p-4 bg-[#F9F9F9] rounded-xl">
+      <div className="grid grid-cols-6 gap-10">
+        {/* Left Side */}
+        <div className=" col-span-4">
+          <div className="bg-white rounded-xl  shadow">
+            <ProfileAvatarCard />
+          </div>
+        </div>
+
+        {/* Right Side  */}
+        <div className="bg-blue-600 col-span-2"> Right Side</div>
+      </div>
     </div>
   );
 };
